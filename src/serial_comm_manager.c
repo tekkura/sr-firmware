oist/smartphone-robot-firmware

Reduce motor command response latency
Avoid full state collection on SET_MOTOR_LEVEL responses by returning only the fast motor state needed by the Android loop. Drain a bounded batch of serial packets per scheduler pass and only sleep when idle so queued motor commands are serviced promptly. Fix the packet read loop bound and share response serialization for state responses.

src/serial_comm_manager.c
@@ -12,6 +12,17 @@ static OutgoingLogPacketToAndroid outgoing_log_packet_to_android;
 void handle_packet(IncomingPacketFromAndroid *packet);
 static RP2040_STATE rp2040_state_;
 
+static void send_state_response(uint8_t packet_type)
+{
+    outgoing_packet_to_android.packet_type = packet_type;
+    outgoing_packet_to_android.data = rp2040_state_;
+
+    uint8_t* bytes = (uint8_t*)&outgoing_packet_to_android;
+    for (int i = 0; i < sizeof(outgoing_packet_to_android); i++){
+        putchar(bytes[i]);
+    }
+}
+
 void serial_comm_manager_init(RP2040_STATE* rp2040_state){
     rp2040_state_ = *rp2040_state;
     incoming_packet_from_android.start_marker = START_MARKER;
@@ -39,7 +50,7 @@ static void reset_packet_and_send_nack(int8_t *start_idx, int8_t *end_idx, uint1
 // reads data from the UART and stores it in buffer. If no data is available, returns immediately.
 // if new data is available, reads it until the buffer is full or both start and stop markers detected
 // calls handle_block to process the data if both markers are detected
-void get_block() {
+bool get_block(void) {
     // initialize as -1 as a way of detecting the absence of each marker in the buffer
     static int8_t start_idx = -1;
     static int8_t end_idx = -1;
@@ -54,7 +65,7 @@ void get_block() {
 	// After finding the start marker get the rest of the packet or until MAX_SERIAL_GET_COUNT
 	// to prevent an infinite loop
 	// + 1 to accomodate for the packet_type
-        while (buffer_index < (ANDROID_BUFFER_LENGTH_IN + 1) || i == MAX_SERIAL_GET_COUNT) {
+        while (buffer_index < (ANDROID_BUFFER_LENGTH_IN + 1) && i < MAX_SERIAL_GET_COUNT) {
             c = getchar_timeout_us(100);
     
     	    if (c != PICO_ERROR_TIMEOUT){
@@ -70,38 +81,40 @@ void get_block() {
     	    }else {
     	        rp2040_log("Timeout while reading packet. Resetting state.\n");
                 reset_packet_and_send_nack(&start_idx, &end_idx, &buffer_index);
-                return;
+                return true;
 	    }
     	    i++;
         }
+
+        if (buffer_index < (ANDROID_BUFFER_LENGTH_IN + 1)) {
+            rp2040_log("Received incomplete packet. Resetting state.\n");
+            reset_packet_and_send_nack(&start_idx, &end_idx, &buffer_index);
+            return true;
+        }
+
 	c = getchar_timeout_us(100);
         if (c != PICO_ERROR_TIMEOUT && c == END_MARKER){
-            // Calculate the length of the packet
-            uint16_t packet_length = end_idx - start_idx;
-            if (packet_length >= sizeof(IncomingPacketFromAndroid)) {
-                // Call the handle_block function with the packet data
-                handle_packet(&incoming_packet_from_android);
-                // Reset the values of start and end idx to detect the next block
-                start_idx = -1;
-                end_idx = -1;
-                buffer_index = 0;
-                // Reset the packet
-                memset(&incoming_packet_from_android, 0, sizeof(IncomingPacketFromAndroid)); } else {
-                rp2040_log("Received incomplete packet. Resetting state.\n");
-                reset_packet_and_send_nack(&start_idx, &end_idx, &buffer_index);
-                return;
-            }
+            // Call the handle_block function with the packet data
+            handle_packet(&incoming_packet_from_android);
+            // Reset the values of start and end idx to detect the next block
+            start_idx = -1;
+            end_idx = -1;
+            buffer_index = 0;
+            // Reset the packet
+            memset(&incoming_packet_from_android, 0, sizeof(IncomingPacketFromAndroid));
         }else{
             rp2040_log("Received packet with no end marker. Resetting state.\n");
             reset_packet_and_send_nack(&start_idx, &end_idx, &buffer_index);
-            return;
+            return true;
         }
 
+        return true;
     }
+
+    return false;
 }
 
 void handle_packet(IncomingPacketFromAndroid *packet){
-    uint8_t* bytes;
     // Assign the same packet type to the outgoing packet for verification on Android end
     switch (packet->packet_type){
     	case GET_LOG:
@@ -123,36 +136,18 @@ void handle_packet(IncomingPacketFromAndroid *packet){
 	    putchar(outgoing_log_packet_to_android.end_marker);
 	    break;
 	case SET_MOTOR_LEVEL:
-            outgoing_packet_to_android.packet_type = packet->packet_type;
-	    // Clear the state
-            memset(&rp2040_state_, 0, sizeof(rp2040_state_));
-	    // Copy the motor levels from the packet to the rp2040_state_
             memcpy(&rp2040_state_.MotorsState.ControlValues.left, &packet->data[0], sizeof(uint8_t));
 	    memcpy(&rp2040_state_.MotorsState.ControlValues.right, &packet->data[1], sizeof(uint8_t));
 	    set_motor_levels(&rp2040_state_);
-            // Add STATE to response
-            get_state(&rp2040_state_);
-	    outgoing_packet_to_android.data = rp2040_state_;
-
-	    // Print the outgoing packet chars
-            bytes = (uint8_t*)&outgoing_packet_to_android;
-            for (int i = 0; i < sizeof(outgoing_packet_to_android); i++){
-                putchar(bytes[i]);
-            }
+            get_fast_motor_state(&rp2040_state_);
+            send_state_response(packet->packet_type);
 	    break;
 	case GET_STATE:
-            outgoing_packet_to_android.packet_type = packet->packet_type;
 	    // Clear the state
             memset(&rp2040_state_, 0, sizeof(rp2040_state_));
             // Add STATE to response
             get_state(&rp2040_state_);
-	    outgoing_packet_to_android.data = rp2040_state_;
-
-	    // Print the outgoing packet chars
-            bytes = (uint8_t*)&outgoing_packet_to_android;
-            for (int i = 0; i < sizeof(outgoing_packet_to_android); i++){
-                putchar(bytes[i]);
-            }
+            send_state_response(packet->packet_type);
 	    break;
 	case RESET_STATE:
 		// TODO

tekkura/sr-firmware

src/serial_comm_manager.c
@@ -12,6 +12,17 @@ static OutgoingLogPacketToAndroid outgoing_log_packet_to_android;
 void handle_packet(IncomingPacketFromAndroid *packet);
 static RP2040_STATE rp2040_state_;
 
+static void send_state_response(uint8_t packet_type)
+{
+    outgoing_packet_to_android.packet_type = packet_type;
+    outgoing_packet_to_android.data = rp2040_state_;
+
+    uint8_t* bytes = (uint8_t*)&outgoing_packet_to_android;
+    for (int i = 0; i < sizeof(outgoing_packet_to_android); i++){
+        putchar(bytes[i]);
+    }
+}
+
 void serial_comm_manager_init(RP2040_STATE* rp2040_state){
     rp2040_state_ = *rp2040_state;
     incoming_packet_from_android.start_marker = START_MARKER;
@@ -39,7 +50,7 @@ static void reset_packet_and_send_nack(int8_t *start_idx, int8_t *end_idx, uint1
 // reads data from the UART and stores it in buffer. If no data is available, returns immediately.
 // if new data is available, reads it until the buffer is full or both start and stop markers detected
 // calls handle_block to process the data if both markers are detected
-void get_block() {
+bool get_block(void) {
     // initialize as -1 as a way of detecting the absence of each marker in the buffer
     static int8_t start_idx = -1;
     static int8_t end_idx = -1;
@@ -54,7 +65,7 @@ void get_block() {
 	// After finding the start marker get the rest of the packet or until MAX_SERIAL_GET_COUNT
 	// to prevent an infinite loop
 	// + 1 to accomodate for the packet_type
-        while (buffer_index < (ANDROID_BUFFER_LENGTH_IN + 1) || i == MAX_SERIAL_GET_COUNT) {
+        while (buffer_index < (ANDROID_BUFFER_LENGTH_IN + 1) && i < MAX_SERIAL_GET_COUNT) {
             c = getchar_timeout_us(100);
     
     	    if (c != PICO_ERROR_TIMEOUT){
@@ -70,38 +81,40 @@ void get_block() {
     	    }else {
                 rp2040_log_w("Timeout while reading packet. Resetting state.\n");
                 reset_packet_and_send_nack(&start_idx, &end_idx, &buffer_index);
-                return;
+                return true;
 	    }
     	    i++;
         }
+
+        if (buffer_index < (ANDROID_BUFFER_LENGTH_IN + 1)) {
+            rp2040_log_w("Received incomplete packet. Resetting state.\n");
+            reset_packet_and_send_nack(&start_idx, &end_idx, &buffer_index);
+            return true;
+        }
+
 	c = getchar_timeout_us(100);
         if (c != PICO_ERROR_TIMEOUT && c == END_MARKER){
-            // Calculate the length of the packet
-            uint16_t packet_length = end_idx - start_idx;
-            if (packet_length >= sizeof(IncomingPacketFromAndroid)) {
-                // Call the handle_block function with the packet data
-                handle_packet(&incoming_packet_from_android);
-                // Reset the values of start and end idx to detect the next block
-                start_idx = -1;
-                end_idx = -1;
-                buffer_index = 0;
-                // Reset the packet
-                memset(&incoming_packet_from_android, 0, sizeof(IncomingPacketFromAndroid)); } else {
-                rp2040_log_w("Received incomplete packet. Resetting state.\n");
-                reset_packet_and_send_nack(&start_idx, &end_idx, &buffer_index);
-                return;
-            }
+            // Call the handle_block function with the packet data
+            handle_packet(&incoming_packet_from_android);
+            // Reset the values of start and end idx to detect the next block
+            start_idx = -1;
+            end_idx = -1;
+            buffer_index = 0;
+            // Reset the packet
+            memset(&incoming_packet_from_android, 0, sizeof(IncomingPacketFromAndroid));
         }else{
             rp2040_log_w("Received packet with no end marker. Resetting state.\n");
             reset_packet_and_send_nack(&start_idx, &end_idx, &buffer_index);
-            return;
+            return true;
         }
 
+        return true;
     }
+
+    return false;
 }
 
 void handle_packet(IncomingPacketFromAndroid *packet){
-    uint8_t* bytes;
     // Assign the same packet type to the outgoing packet for verification on Android end
     switch (packet->packet_type){
     	case GET_LOG:
@@ -123,36 +136,18 @@ void handle_packet(IncomingPacketFromAndroid *packet){
 	    putchar(outgoing_log_packet_to_android.end_marker);
 	    break;
 	case SET_MOTOR_LEVEL:
-            outgoing_packet_to_android.packet_type = packet->packet_type;
-	    // Clear the state
-            memset(&rp2040_state_, 0, sizeof(rp2040_state_));
-	    // Copy the motor levels from the packet to the rp2040_state_
             memcpy(&rp2040_state_.MotorsState.ControlValues.left, &packet->data[0], sizeof(uint8_t));
 	    memcpy(&rp2040_state_.MotorsState.ControlValues.right, &packet->data[1], sizeof(uint8_t));
 	    set_motor_levels(&rp2040_state_);
-            // Add STATE to response
-            get_state(&rp2040_state_);
-	    outgoing_packet_to_android.data = rp2040_state_;
-
-	    // Print the outgoing packet chars
-            bytes = (uint8_t*)&outgoing_packet_to_android;
-            for (int i = 0; i < sizeof(outgoing_packet_to_android); i++){
-                putchar(bytes[i]);
-            }
+            get_fast_motor_state(&rp2040_state_);
+            send_state_response(packet->packet_type);
 	    break;
 	case GET_STATE:
-            outgoing_packet_to_android.packet_type = packet->packet_type;
 	    // Clear the state
             memset(&rp2040_state_, 0, sizeof(rp2040_state_));
             // Add STATE to response
             get_state(&rp2040_state_);
-	    outgoing_packet_to_android.data = rp2040_state_;
-
-	    // Print the outgoing packet chars
-            bytes = (uint8_t*)&outgoing_packet_to_android;
-            for (int i = 0; i < sizeof(outgoing_packet_to_android); i++){
-                putchar(bytes[i]);
-            }
+            send_state_response(packet->packet_type);
 	    break;
 	case RESET_STATE:
 		// TODO